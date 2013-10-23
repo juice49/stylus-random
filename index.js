af135a2e2f3f44ff1598e68eca63c0db4e3c8bb2
Ash <ashley@chichesterdesign.co.uk>
2013-10-23 13:10:52 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+
+	return function(style) {
+
+		style.define('random', function(min, max) {
+			if(min === undefined || max === undefined) {
+				return Math.random();
+			}
+			return Math.floor(Math.random() * (Number(max) - Number(min) + 1)) + Number(min);
+		});
+
+	}
+
+};
